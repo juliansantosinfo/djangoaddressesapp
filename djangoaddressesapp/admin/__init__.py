1b4c9047e29c3d93f5ddd670a55ae1845a4e7a5b
Julian de Almeida Santos <julian.santos.info@gmail.com>
2021-02-25 19:47:29 -0300

    Refactory project.

--- djangoaddressesapp/admin/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .RegionAdmin import RegionAdmin
-from .StateAdmin import StateAdmin
-from .CityAdmin import CityAdmin
-from .AddressAdmin import AddressAdmin
--- djangoaddressesapp/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-from .Region import Region
-from .State import State
-from .City import City
-from .Address import Address
