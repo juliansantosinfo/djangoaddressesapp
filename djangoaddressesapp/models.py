960bf53383ae0d6159f94211cd11f225842a22b1
Julian Santos <julian.santos.info@gmail.com.br>
2021-08-24 21:20:32 -0300

    Moved import functions to models file.

--- djangoaddressesapp/models.py
+++ djangoaddressesapp/models.py
@@ -1,3 +1,4 @@
+import requests
 from django.db import models
 from django.utils.translation import gettext, gettext_lazy as _
 
@@ -122,3 +123,52 @@ class Address(models.Model):
 
     def __str__(self):
         return self.address
+
+
+def import_regions():
+    """
+    docstring
+    """
+
+    response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
+    data = response.json()
+
+    for item in data:
+        region = Region()
+        region.pk = item.get("id")
+        region.name = item.get("nome")
+        region.acronym = item.get("sigla")
+        region.save()
+
+
+def import_state():
+    """
+    docstring
+    """
+
+    response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    data = response.json()
+
+    for item in data:
+        state = State()
+        state.pk = item.get("id")
+        state.name = item.get("nome")
+        state.acronym = item.get("sigla")
+        state.region = Region.objects.get(pk=item.get("regiao").get("id"))
+        state.save()
+
+
+def import_cities():
+    """
+    docstring
+    """
+
+    response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/municipios')
+    data = response.json()
+
+    for item in data:
+        city = City()
+        city.pk = item.get("id")
+        city.name = item.get("nome")
+        city.state = State.objects.get(pk=item.get("microrregiao").get("mesorregiao").get("UF").get("id"))
+        city.save()
